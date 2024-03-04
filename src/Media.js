668268419d7b8fdc6e0f6b42cb17153f4d2dce46
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-04 12:37:43 +0330

    create project

---
+++ src/Media.js
@@ -0,0 +1,13 @@
+function Media(){
+    return(
+        <div>
+            <ul>
+                <li>Telegram</li>
+                <li>Instagram</li>
+                <li>twiteer</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Media
