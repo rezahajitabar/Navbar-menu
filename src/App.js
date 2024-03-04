24006a6e1fb716065af1807cece4115f4e7abe6e
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-04 16:49:06 +0330

    two commit project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import "./Application.css";
 
 function App() {
   return (
     <div>
-       
+       <div class='nav-background'>
+        <div class='nav-container'>
+          <header class='nav-header'>
+            <h1><a href='#' class='nav-logo'>SignalMobile</a></h1>
+          </header>
+          <nav>
+            <ul class='nav-ul'>
+              <li><a href='#' class='nav-link'>Home</a></li>
+              <li><a href='#' class='nav-link'>News</a></li>
+              <li><a href='#' class='nav-link'>Contact</a></li>
+              <li><a href='#' class='nav-link'>About</a></li>
+            </ul>
+          </nav>
+        </div>
+       </div>
     </div>
   );
 }
